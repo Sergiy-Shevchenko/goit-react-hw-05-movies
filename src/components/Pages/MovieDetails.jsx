86f3e6f2512e6e3b7240ea8a9e931a172677c205
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-06-01 13:13:43 +0300

    Update MovieDetails.jsx

--- src/components/Pages/MovieDetails.jsx
+++ src/components/Pages/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MovieDetails from "components/MovieCard/MovieCard";
+import {MovieDetails} from "components/MovieCard/MovieCard";
 
 const MovieDetails = () => {
     return (
